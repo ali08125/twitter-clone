dfa60a0c6994c1aaa548e4439b4ac717acc7f747
Ali <mirzaeiali769@gmail.com>
2023-10-29 09:31:32 +0330

    add users collection id to appwrite constants

--- lib/common/constants/appwrite_constants.dart
+++ lib/common/constants/appwrite_constants.dart
@@ -2,4 +2,5 @@ class AppwriteConstants {
   static const databaseId = '653b4cce49b1ee88b687';
   static const projectId = '653b47cab0307b6ed8af';
   static const endPoint = 'https://cloud.appwrite.io/v1';
+  static const usersCollectionId = '653df29f26b9bd8ee502';
 }
